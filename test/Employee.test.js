363e229a625a2bbd1de6bf19a413ce527f44ef83
Svitlana Zaster <szaster@protonmail.com>
2020-06-22 14:45:35 -0500

    Skipped all tests that I'm not currently working on

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,8 +1,8 @@
 const Employee = require("../lib/Employee");
 
-test.skip("Can instantiate Employee instance", () => {
+test("Can instantiate Employee instance", () => {
   const e = new Employee();
-  expect(typeof(e)).toBe("object");
+  expect(typeof e).toBe("object");
 });
 
 test("Can set name via constructor arguments", () => {
@@ -11,37 +11,37 @@ test("Can set name via constructor arguments", () => {
   expect(e.name).toBe(name);
 });
 
-test("Can set id via constructor argument", () => {
+test.skip("Can set id via constructor argument", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
   expect(e.id).toBe(testValue);
 });
 
-test("Can set email via constructor argument", () => {
+test.skip("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
   expect(e.email).toBe(testValue);
 });
 
-test("Can get name via getName()", () => {
+test.skip("Can get name via getName()", () => {
   const testValue = "Alice";
   const e = new Employee(testValue);
   expect(e.getName()).toBe(testValue);
 });
 
-test("Can get id via getId()", () => {
+test.skip("Can get id via getId()", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
   expect(e.getId()).toBe(testValue);
 });
 
-test("Can get email via getEmail()", () => {
+test.skip("Can get email via getEmail()", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
   expect(e.getEmail()).toBe(testValue);
 });
 
-test("getRole() should return \"Employee\"", () => {
+test.skip('getRole() should return "Employee"', () => {
   const testValue = "Employee";
   const e = new Employee("Alice", 1, "test@test.com");
   expect(e.getRole()).toBe(testValue);
