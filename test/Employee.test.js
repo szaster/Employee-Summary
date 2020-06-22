e8b3e43303072419112d5ee0dd370ff7044aa900
Svitlana Zaster <szaster@protonmail.com>
2020-06-22 15:16:25 -0500

    Passed all Employee tests

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -23,25 +23,25 @@ test("Can set email via constructor argument", () => {
   expect(e.email).toBe(testValue);
 });
 
-test.skip("Can get name via getName()", () => {
+test("Can get name via getName()", () => {
   const testValue = "Alice";
   const e = new Employee(testValue);
   expect(e.getName()).toBe(testValue);
 });
 
-test.skip("Can get id via getId()", () => {
+test("Can get id via getId()", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
   expect(e.getId()).toBe(testValue);
 });
 
-test.skip("Can get email via getEmail()", () => {
+test("Can get email via getEmail()", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
   expect(e.getEmail()).toBe(testValue);
 });
 
-test.skip('getRole() should return "Employee"', () => {
+test('getRole() should return "Employee"', () => {
   const testValue = "Employee";
   const e = new Employee("Alice", 1, "test@test.com");
   expect(e.getRole()).toBe(testValue);
